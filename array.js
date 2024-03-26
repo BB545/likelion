205a6a49046611a0249c3397945e2edaa0a0db1a
노현희 <148606294+BB545@users.noreply.github.com>
2024-03-26 20:22:04 +0900

    likelion: 1차 정기세션 - js 연습

--- array.js
+++ array.js
@@ -5,3 +5,16 @@ console.log(arr[1][0]);
 console.log(arr[1][1]);
 console.log(arr[2][0]);
 console.log(arr[2][1]);
+
+const num=100;
+
+if(num==100){
+    document.write("실행되었습니다.(true)");
+}
+
+if(num==100) document.write("실행되었습니다.(true)");
+
+if(num==100) document.write("실행되었습니다.(true)");
+else document.write("실행되었습니다.(false)");
+
+(num==100)?document.write("true"):document.write(false);
