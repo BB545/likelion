11e02e890e109c8dbe87f762249ae61751c95aa4
BB545 <black1016@hanmail.net>
2024-08-09 09:15:28 +0900

    likelion: 5주차 세션 - 댓글기능 추가

--- 05_session/write/src/components/Article.jsx
+++ 05_session/write/src/components/Article.jsx
@@ -5,6 +5,15 @@ import HeartFull from '../assets/img/heartfull.svg'
 
 const Article = ({ title, content }) => {
     const [click, setClick] = useState(false);
+    const [commentTxt, setCommentTxt] = useState('');
+    const [comment, setComment] = useState([]);
+
+    const handleComments = () => {
+        if (commentTxt.length > 0) {
+            setComment((prev) => [...prev, commentTxt]);
+            setCommentTxt('');
+        }
+    }
 
     return (
         <div className='article_wrap'>
@@ -31,9 +40,16 @@ const Article = ({ title, content }) => {
                 </div>
             </div>
             <div className="comment">
-                <input type="text" placeholder='댓글을 입력해주세요!' />
-                <button>등록</button>
+                <input type="text" placeholder='댓글을 입력해주세요!' value={commentTxt} onChange={(e) => setCommentTxt(e.target.value)} />
+                <button onClick={handleComments}>등록</button>
             </div>
+            {comment.length > 0 ? comment.map((com) => (
+                <div className='commentBox'>
+                    {com}
+                </div>
+            )) : (
+                <div style={{textAlign: "center"}}>작성된 댓글이 없습니다.</div>
+            ) }
         </div>
     )
 }
