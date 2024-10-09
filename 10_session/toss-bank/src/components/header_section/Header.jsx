8cf0eb9d99b8a8de83c6486affb4388bbfe62ccc
BB545 <black1016@hanmail.net>
2024-10-09 23:35:40 +0900

    likelion: 10주차 세션 - tossbank 파비콘 수정 및  헤더 nav 구현 중

--- 10_session/toss-bank/src/components/header_section/Header.jsx
+++ 10_session/toss-bank/src/components/header_section/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import tossbank from '../../assets/images/logo/TossBank_Logo.svg'
 import Nav from './Nav';
+import NavToggle from './NavToggle';
 
 const Header = () => {
   return (
     <header id='header_container'>
-        <img src={tossbank} alt="토스뱅크 로고" id='tossbank_logo' className='btn' />
-        <Nav />
-        <div id="header_start_btn" className='btn'>시작하기</div>
+        <div id="header_container_top">
+          <img src={tossbank} alt="토스뱅크 로고" id='tossbank_logo' className='btn' />
+          <Nav />
+          <div id="header_start_btn" className='btn'>시작하기</div>
+        </div>
+        <NavToggle />
     </header>
   )
 }
