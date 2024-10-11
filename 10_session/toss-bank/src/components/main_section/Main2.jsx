149c7005226cfa64f536403b77b74ec4f45bd970
BB545 <black1016@hanmail.net>
2024-10-11 21:23:05 +0900

    likelion: toss-bank 메인 2 글자 색 변환 애니메이션 구현 (개선 필요)

---
+++ 10_session/toss-bank/src/components/main_section/Main2.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+const Main2 = () => {
+
+  return (
+    <div id='main_2_container'>
+        <div id="main_2_inner_container">
+            <section id="main_2_section_title">
+                <h3 id="title_black">나에게 맞게 언제든지</h3>
+                <div id="title_color_area">
+                    <div id="title_color_box">
+                        <div id="title_color">
+                            <h3 id="title_color_line">
+                                <div className="color_word">
+                                    <div className="color_char char1">혜</div>
+                                    <div className="color_char char2">택</div>
+                                    <div className="color_char char3">,&nbsp;</div>
+                                </div>
+                                <div className="color_word">
+                                    <div className="color_char char4">내</div>
+                                    <div className="color_char char5">가&nbsp;</div>
+                                </div>
+                                <div className="color_word">
+                                    <div className="color_char char6">선</div>
+                                    <div className="color_char char7">택</div>
+                                    <div className="color_char char8">.</div>
+                                </div>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div id="empty"></div>
+                <span id="subtitle_gray">토스뱅크 체크카드 스위치 캐시백 시즌3</span>
+            </section>
+        </div>
+    </div>
+  )
+}
+
+export default Main2
