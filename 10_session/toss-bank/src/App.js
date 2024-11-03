56d966d80736ebfc28aaeef13590ff40b2409b46
BB545 <black1016@hanmail.net>
2024-11-04 03:03:03 +0900

    likelion: toss-bank footer 구현 완료

--- 10_session/toss-bank/src/App.js
+++ 10_session/toss-bank/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header_section/Header'
 import Main from './pages/Main';
+import Footer from './components/footer_section/Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   )
 }
---
+++ 10_session/toss-bank/src/components/footer_section/Footer.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer id='footer_container'>
+            <div id="footer_inner">
+                <div id="inner_group_1">
+                    <ul id="group_1_inner">
+                        <li id="group_1_inner_li">
+                            <div id="inner_li_1">고객센터</div>
+                        </li>
+                        <ul id="group_1_inner_ul">
+                            <li className="inner_li_2">
+                                <a href="tel:1661-7654">전화 : 1661-7654(24시간 연중무휴)</a>
+                            </li>
+                            <li className="inner_li_2">
+                                <a href="tel:1661-7654">해외전화 : +82-2-6975-9000</a>
+                            </li>
+                            <li className="inner_li_2">
+                                <a href="mailto:help@tossbank.com">이메일 : help@tossbank.com</a>
+                            </li>
+                        </ul>
+                    </ul>
+                </div>
+                <address id='inner_group_2'>
+                    <strong id='group_2_inner_strong'>토스뱅크㈜</strong>
+                    <div id="group_2_inner_list">
+                        <span className="inner_list_1">사업자 등록번호 : 462-86-01671</span>
+                        <span className="inner_list_1">주소 : 06133 서울특별시 강남구 테헤란로 131, 13층 (역삼동, 한국지식재산센터)</span>
+                        <span className="inner_list_1">대표 : 이은미</span>
+                    </div>
+                </address>
+                <ul id="inner_group_3">
+                    <li className="group_3_li_1">
+                        <a href="">
+                            <strong>개인정보 처리방침</strong>
+                        </a>
+                    </li>
+                    <li className="group_3_li_1">
+                        <a href="">보호금융상품등록부</a>
+                    </li>
+                    <li className="group_3_li_1">
+                        <a href="">이용자유의사항</a>
+                    </li>
+                    <li className="group_3_li_1">
+                        <a href="">공지사항</a>
+                    </li>
+                    <li className="group_3_li_1">
+                        <a href="">상품공시실</a>
+                    </li>
+                    <li className="group_3_li_1">
+                        <a href="">경영공시</a>
+                    </li>
+                </ul>
+                <ul id="inner_group_4">
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCJwvlCWreTTO2IAPS8hEfKQ">
+                            <div className='group_4_inner'>
+                                <div className="group_4_inner_box">
+                                    <span className="inner_box_span_area">
+                                        <span className="inner_box_span">
+                                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+                                                <path d="m21.679 7.192c-.286-.952-.952-1.619-1.809-1.809-1.143-.476-12.378-.666-15.615 0-.952.286-1.619.952-1.809 1.809-.571 2.285-.571 7.236 0 9.617.19.857.857 1.523 1.619 1.809 2.285.571 13.14.571 15.615 0 .952-.286 1.619-.952 1.809-1.809.571-2.476.667-7.141.19-9.617zm-11.039 7.857v-5.903l5.142 2.952-5.142 2.952z" fill="#b0b8c1" />
+                                            </svg>
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://pf.kakao.com/_xglreb">
+                            <div className='group_4_inner'>
+                                <div className="group_4_inner_box">
+                                    <span className="inner_box_span_area">
+                                        <span className="inner_box_span">
+                                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+                                                <path d="m11.999 2c-5.222 0-9.455 3.915-9.455 8.746 0 3.567 1.494 5.27 3.541 6.823l.009.004v4.187c0 .198.226.311.385.192l3.586-2.663.078.034c.601.11 1.222.169 1.859.169 5.223 0 9.456-3.916 9.456-8.747s-4.236-8.745-9.459-8.745zm-3.169 10.961c.927 0 1.72-.555 1.958-1.412h1.371c-.335 1.65-1.619 2.731-3.329 2.731-1.988 0-3.563-1.482-3.563-3.545s1.576-3.545 3.563-3.545c1.727 0 3.02 1.103 3.339 2.781h-1.367c-.208-.912-1.028-1.481-1.972-1.481-1.336 0-2.225.953-2.225 2.245s1.011 2.226 2.225 2.226zm9.033 1.184h-1.257v-2.835c0-.652-.38-1.003-.964-1.003-.652 0-1.072.399-1.072 1.207v2.63h-1.257v-6.932h1.257v2.608c.302-.457.789-.681 1.451-.681.536 0 .974.175 1.314.536.351.36.526.847.526 1.48v2.99h.001z" fill="#b0b8c1" />
+                                            </svg>
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/tossbank.official/">
+                            <div className='group_4_inner'>
+                                <div className="group_4_inner_box">
+                                    <span className="inner_box_span_area">
+                                        <span className="inner_box_span">
+                                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+                                                <g fill='#b0b8c1'>
+                                                    <path d="m17.421 5.466c.623 0 1.126.505 1.126 1.126s-.503 1.126-1.126 1.126-1.125-.505-1.125-1.126.503-1.126 1.125-1.126z" />
+                                                    <path d="m12.035 7.102c2.711 0 4.917 2.205 4.917 4.917s-2.206 4.917-4.917 4.917-4.917-2.206-4.917-4.917 2.206-4.917 4.917-4.917zm0 1.923c-1.65 0-2.994 1.342-2.994 2.994s1.344 2.992 2.994 2.992 2.992-1.342 2.992-2.992-1.342-2.994-2.992-2.994z" />
+                                                    <path d="m16.345 2c3.118 0 5.655 2.537 5.655 5.655v8.689c0 3.118-2.537 5.655-5.655 5.655h-8.69c-3.118 0-5.655-2.537-5.655-5.655v-8.689c0-3.118 2.537-5.655 5.655-5.655h8.689zm0 1.923h-8.69c-2.058 0-3.73 1.674-3.73 3.732v8.689c0 2.058 1.673 3.732 3.73 3.732h8.689c2.058 0 3.732-1.674 3.732-3.732v-8.689c0-2.058-1.674-3.732-3.732-3.732z" />
+                                                </g>
+                                            </svg>
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://x.com/toss__bank">
+                            <div className='group_4_inner'>
+                                <div className="group_4_inner_box">
+                                    <span className="inner_box_span_area">
+                                        <span className="inner_box_span">
+                                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+                                                <path d="m13.716 10.608 6.588-7.608h-1.578l-5.66 6.588-4.546-6.588h-5.382l6.866 10.02-6.866 7.98h1.578l6.031-6.959 4.825 6.959h5.289l-7.145-10.392h-.001zm-2.134 2.505-.742-1.021-5.567-7.98h2.413l4.453 6.402.742 1.021 5.846 8.351h-2.413z" fill="#b0b8c1" />
+                                            </svg>
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
