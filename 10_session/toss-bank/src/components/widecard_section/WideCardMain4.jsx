155e4a8e697bc77ffbe4c75ffc0b43b3f7e790fa
BB545 <black1016@hanmail.net>
2024-11-04 17:42:08 +0900

    likelion: toss-bank 와이드 카드 메인 4 구현 완료

---
+++ 10_session/toss-bank/src/components/widecard_section/WideCardMain4.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const WideCardMain4 = () => {
+    return (
+        <div id='wide_card_main_4_container'>
+            <div id="wide_4_inner_container">
+                <div id='wide_4_inner_box'>
+                    <img src="https://static.toss.im/3d/money-bag-bill-coin.png" alt="" id="wide_4_inner_img" />
+                    <div id="wide_4_inner_content">
+                        <span id='wide_4_inner_badge'>월 최대 10만원 할인</span>
+                        <div id='blank_12'></div>
+                        <span id='wide_4_inner_title'>
+                            연 최대
+                            <br />
+                            120만원 할인
+                        </span>
+                        <div id='blank_13'></div>
+                        <div id='wide_4_inner_table'>
+                            <div className="wide_4_inner_table_item">
+                                <span className='wide_4_table_1'>전월실적</span>
+                                <div className='blank_14'></div>
+                                <span className='wide_4_table_2'>
+                                    40만원 이상 2% 청구할인
+                                    <br />
+                                    40만원 미만 1% 청구할인
+                                </span>
+                            </div>
+                            <div className="wide_4_inner_table_item">
+                                <span className='wide_4_table_1'>연회비</span>
+                                <div className='blank_14'></div>
+                                <span className='wide_4_table_2'>
+                                    국내전용・국내외겸용
+                                    <br />
+                                    2만원 (Mastercard)
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='blank_10'></div>
+                <span id='wide_4_innner_des'>✱ 일시불 결제만 할인 적용 (할인 제외 대상은 상품약관 참고)</span>
+                <div id='blank_11'></div>
+            </div>
+        </div>
+    )
+}
+
+export default WideCardMain4
--- 10_session/toss-bank/src/pages/WideCard.jsx
+++ 10_session/toss-bank/src/pages/WideCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import WideCardMain1 from '../components/widecard_section/WideCardMain1'
 import WideCardMain2 from '../components/widecard_section/WideCardMain2'
 import WideCardMain3 from '../components/widecard_section/WideCardMain3'
+import WideCardMain4 from '../components/widecard_section/WideCardMain4'
 
 const WideCard = () => {
   return (
@@ -9,6 +10,7 @@ const WideCard = () => {
         <WideCardMain1 />
         <WideCardMain2 />
         <WideCardMain3 />
+        <WideCardMain4 />
     </main>
   )
 }
