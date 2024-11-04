67aba836d92250731327099927fa4dca9f9a1673
BB545 <black1016@hanmail.net>
2024-11-04 17:04:08 +0900

    likelion: toss-bank 와이드 카드 메인 2 구현 완료

---
+++ 10_session/toss-bank/src/components/widecard_section/WideCardMain2.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const WideCardMain2 = () => {
+  return (
+    <div id='wide_card_main_2_container'>
+        <div id="wide_2_inner_container">
+            <div id="wide_2_inner_box">
+                <img src="https://static.toss.im/3d/uE116-2-percent.png" alt="" id="wide_2_inner_img" />
+                <div id="wide_2_inner_content">
+                    <span id='wide_2_inner_subtitle'>국내외 전 가맹점</span>
+                    <div id='blank_6'></div>
+                    <span id='wide_2_inner_title'>결제할 때마다 할인</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WideCardMain2
