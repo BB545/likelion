ad89cc3f928fe7268ba34b6b23207f2efd1d387d
BB545 <black1016@hanmail.net>
2024-11-04 17:17:22 +0900

    likelion: toss-bank 와이드 카드 메인 3 구현 완료

---
+++ 10_session/toss-bank/src/components/widecard_section/WideCardMain3.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const WideCardMain3 = () => {
+  return (
+    <div id='wide_card_main_3_container'>
+        <div id="wide_3_inner_container">
+            <div id="wide_3_inner_box">
+                <div id="wide_3_inner_content">
+                    <span className='wide_3_inner_badge'>전월실적 40만원 채우면</span>
+                    <div className='blank_8'></div>
+                    <span className='wide_3_inner_title'>2% 할인</span>
+                    <div id='blank_9'></div>
+                    <span className='wide_3_inner_badge'>전월실적 없어도</span>
+                    <div className='blank_8'></div>
+                    <span className='wide_3_inner_title'>기본 1% 할인</span>
+                </div>
+                <img src="https://static.toss.im/3d/tosscard-coin-wallet.png" alt="" id="wide_3_inner_img" />
+            </div>
+        </div>
+        <div id="blank_7"></div>
+    </div>
+  )
+}
+
+export default WideCardMain3
--- 10_session/toss-bank/src/pages/WideCard.jsx
+++ 10_session/toss-bank/src/pages/WideCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import WideCardMain1 from '../components/widecard_section/WideCardMain1'
 import WideCardMain2 from '../components/widecard_section/WideCardMain2'
+import WideCardMain3 from '../components/widecard_section/WideCardMain3'
 
 const WideCard = () => {
   return (
     <main id='wide_card_container'>
         <WideCardMain1 />
         <WideCardMain2 />
+        <WideCardMain3 />
     </main>
   )
 }
